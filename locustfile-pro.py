b0bc59fade797307cf199455de5d9ce62a931576
cah-habasse-traore <habasse.traore@cardinalhealth.com>
2021-03-16 23:21:30 -0400

    simple locustfile

--- locustfile-pro.py
+++ locustfile-pro.py
@@ -1,41 +1,25 @@
 import time, random
-# Must be included in all the scripts 
-from locust_plugins import listeners  # NEW
-
-#from locust_plugins.listeners import TimescaleListener - OLD
-
-# Must Import 'events' for script to report metrics to timescale-db
-from locust import HttpUser, TaskSet, task, between, events
+from locust import HttpUser, TaskSet, task, between 
 
 
 class MyWebsiteUser(HttpUser):
     wait_time = between(1, 2)
 
-    # Failed transaction to show failures reported in Grafana
     @task
-    def index(self):
-        self.client.post("/authentication/1.0/getResults", {"postgres": "admin"})
-
-    @task
-    def view_pro_home_page(self):
-        self.client.get("/")
+    def view_main_page(self):
+        self.client.get("/cgi-bin/mncal.cgi?ccoa")
 
     @task
-    def view_pro_docs_page(self):
-        self.client.get("/docs/introduction/overview/")
+    def view_auction_page(self):
+        auction_num = str(random.randint(1,300))
+        self.client.get("/cgi-bin/mndetails.cgi?ccoa"+ auction_num)
 
     @task
-    def view_pro_download_page(self):
-        self.client.get("/download/")
+    def view_all_biditems_page(self):
+        auction_num = str(random.randint(1,300))
+        self.client.get("/cgi-bin/mnlist.cgi?ccoa"+auction_num+"/category/ALL")
 
     @task
-    def view_pro_community_page(self):
-        self.client.get("/community/")
-
-
-# Must be added to Locustfile to report execution metrics to timescale
-# Grafana Dashboard key to view execution specific data is based on the 'testplan' value
-@events.init.add_listener
-def on_locust_init(environment, **_kwargs):
-    listeners.Timescale(env=environment, testplan="taurus_pro_listener_ex", target_env="myTestEnv")
- #  TimescaleListener(env=environment, testplan="taurus_pro_listener_ex", target_env="myTestEnv") - OLD
+    def view_biditem_detail_page(self):
+        auction_num = str(random.randint(1,300))
+        self.client.get("/cgi-bin/mnlist.cgi?ccoa"+auction_num+"/1")
